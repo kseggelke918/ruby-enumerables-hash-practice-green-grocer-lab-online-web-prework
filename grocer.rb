ba0dd7c7cbea88d38bab82ae10668a39ad31b130
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-09 17:30:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ end
 def apply_clearance(cart)
   cart.each do |food, attribute|
     if attribute[:clearance] == true 
-      attribute[:price] = attribute[:price] * 0.8 
+      attribute[:price] = (attribute[:price] * 0.8).round(2) 
     end 
   end 
   return cart
