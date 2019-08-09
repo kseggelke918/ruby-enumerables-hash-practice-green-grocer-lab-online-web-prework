7e1b6b1f594b169977f2d59ad2b7d0d834e64f3b
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-09 17:43:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,7 +50,7 @@ def checkout(cart, coupons)
   checkout_clearance = apply_clearance(checkout_coupon)
   
   checkout_clearance.each do |food, attribute|
-    total = attribute[:price] * attribute[:count]
+    total += attribute[:price] * attribute[:count]
   end 
 end
 
